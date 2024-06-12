0f5d21d4519c84014d45acb18b7b98f4ab79848c
blueice01 <wkdma1524@gmail.com>
2024-06-12 21:42:14 +0900

    layout_01

--- components/component/layout.tsx
+++ components/component/layout.tsx
@@ -48,7 +48,7 @@ export function Layout() {
             <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
               <div>
                 <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                  John Doe
+                3D Artist
                 </h1>
                 <h2 className="text-xl font-medium text-gray-400">Full-Stack Developer</h2>
                 <p className="mt-4 max-w-[700px] text-gray-400 md:text-xl">
